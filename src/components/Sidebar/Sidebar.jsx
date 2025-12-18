693b77a5d0a64639a02d57cacdc72ebbcf84d0b9
Ederish <63983547+Ederish@users.noreply.github.com>
2025-12-17 19:13:30 -0600

    Update Sidebar.jsx
    añade un menu deacordeón al sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,37 +1,97 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { SidebarData } from './SidebarData';
-import './Sidebar.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "./Sidebar.css";
+
+const sections = {
+  "Sobre Nosotros": [
+    "Nuestra Historia",
+    "Junta Directiva",
+    "Observatorio",
+    "Nuestras Actividades",
+    "Únete A Nosotros",
+  ],
+  "Publicaciones": [
+    "Imagen Del Día",
+    "Ponencias",
+    "Podcast",
+    "Revistas",
+    "Videos",
+    "Galería",
+    "Boletines",
+  ],
+  "Eventos": [
+    "Eventos Del Mes",
+    "Calendario Lunar",
+  ],
+};
 
 function Sidebar() {
   const navigate = useNavigate();
 
   const handleNavigation = (item) => {
     if (item.endpoint === "internal") {
-      // Navegación interna, por ejemplo al Home
       navigate(item.link);
     } else {
-      // Si es página o categoría, construimos la URL con parámetros
       navigate(
         `/post/${item.id}?site=https://astro.org.sv&endpoint=${item.endpoint}`
       );
     }
   };
 
+  const getItemsBySection = (titles) =>
+    SidebarData.filter((item) => titles.includes(item.title));
+
   return (
-    <div className="sidebar">
+    <aside className="sidebar">
       <ul className="sidebarList">
-        {SidebarData.map((val, key) => (
-          <li
-            key={key}
-            className="row"
-            onClick={() => handleNavigation(val)}
+        {/* INICIO */}
+        <li className="row">
+          <button
+            className="link-btn"
+            onClick={() => handleNavigation(SidebarData[0])}
           >
-            <div id="title">{val.title}</div>
+            Inicio
+          </button>
+        </li>
+
+        {/* SECCIONES CON DETAILS / SUMMARY */}
+        {Object.entries(sections).map(([sectionTitle, items]) => (
+          <li key={sectionTitle} className="row">
+            <details>
+              <summary>{sectionTitle}</summary>
+
+              <ul className="submenu">
+                {getItemsBySection(items).map((item, index) => (
+                  <li key={index}>
+                    <button
+                      className="submenu-btn"
+                      onClick={() => handleNavigation(item)}
+                    >
+                      {item.title}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </details>
           </li>
         ))}
+
+        {/* ENLACES */}
+        <li className="row">
+          <button
+            className="link-btn"
+            onClick={() =>
+              handleNavigation(
+                SidebarData.find((i) => i.title === "Enlaces")
+              )
+            }
+          >
+            Enlaces
+          </button>
+        </li>
       </ul>
-    </div>
+    </aside>
   );
 }
 
